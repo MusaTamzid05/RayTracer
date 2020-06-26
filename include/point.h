2c1194e833dc907a80db865ac739c6abaa6653dd
MusaTamzid05 <tamzid101@gmail.com>
2020-06-26 19:47:23 +0600

    point operator overload added.

--- include/point.h
+++ include/point.h
@@ -11,8 +11,27 @@ namespace TwoD {
             Point(float x , float y , float z):
                 Tuple(x , y , z , 1.0){}
 
-        static Point convert_to_point(const TwoD::Tuple& data);
+        static Point convert_to_point(const TwoD::Point& data);
+
+
+        void operator+=(const Point& point);
+        void operator-=(const Point& point);
+        void operator*=(float value);
+        void operator/=(float value);
+
+        Point operator*(float value);
+        Point operator/(float value);
+
+        bool operator==(const Point& point);
+        Point normalize();
+
+
     };
+
+    Point operator+(const Point& data1 , const Point& data2);
+    Point operator-(const Point& data1 , const Point& data2);
+    Point operator*(const Point& data1 , const Point& data2);
+
 };
 
 #endif
--- src/point.cpp
+++ src/point.cpp
@@ -1,4 +1,5 @@
 #include "point.h"
+#include "operation.h"
 
 namespace TwoD {
 
@@ -7,4 +8,83 @@ namespace TwoD {
         point.w = data.w;
         return point;
     }
+
+
+    void Point::operator+=(const Point& point) {
+
+        x += point.x;
+        y += point.y;
+        z += point.z;
+    }
+
+
+    void Point::operator-=(const Point& point) {
+
+        x -= point.x;
+        y -= point.y;
+        z -= point.z;
+    }
+
+    void Point::operator*=(float value) {
+
+        x *= value;
+        y *= value;
+        z *= value;
+    }
+
+    
+    void Point::operator/=(float value) {
+
+        x /= value;
+        y /= value;
+        z /= value;
+    }
+
+    
+    bool Point::operator==(const Point& point) {
+
+        return Operation::equal(x , point.x) &&
+            Operation::equal(y , point.y) && 
+            Operation::equal(z , point.z);
+    }
+
+    Point Point::operator*(float value) {
+
+        x *= value;
+        y *= value;
+        z *= value;
+
+        return *this;
+    }
+
+    Point Point::operator/(float value) {
+
+        x /= value;
+        y /= value;
+        z /= value;
+
+        return *this;
+    }
+
+
+    Point operator+(const Point& data1 , const Point& data2) {
+        return Point(data1.x + data2.x , data1.y + data2.y , data1.z + data2.z);
+    }
+
+    Point operator-(const Point& data1 , const Point& data2) {
+        return Point(data1.x - data2.x , data1.y - data2.y , data1.z - data2.z);
+    }
+
+    Point operator*(const Point& data1 , const Point& data2) {
+        return Point(data1.x * data2.x , data1.y * data2.y , data1.z * data2.z);
+    }
+
+
+    Point Point::normalize() {
+
+        float mag = magnitude();
+        return Point(x / mag , y / mag , z / mag);
+
+    }
+
 };
