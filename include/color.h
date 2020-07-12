13eba366d0f683e99a33b0e534b259166e9dd9ff
MusaTamzid05 <tamzid101@gmail.com>
2020-07-13 02:46:38 +0600

    default value added.

--- include/color.h
+++ include/color.h
@@ -7,7 +7,7 @@ namespace Engine {
 
     struct Color: public TwoD::Tuple {
 
-        Color(float r , float g , float b):
+        Color(float r = 0.0f , float g = 0.0f , float b = 0.0f):
             TwoD::Tuple(r , g , b , 0.0f) {}
 
         float red() const  { return x; }
@@ -15,6 +15,7 @@ namespace Engine {
         float blue() const { return z; }
 
         Color  hadamard_product(const Color& color) const;
+        static Color convert_to_color(const Tuple& data);
 
     };
 }
